7b985c15b3c7030b0db533de9358bbe0e8fb6d71
jevitapearl <jevitapearl@gmail.com>
2025-07-30 11:30:13 +0000

    removing duplicates

--- practice-projects/hello-world/script.js
+++
@@ -1,7 +0,0 @@
-let btnElement = document.querySelector("button");
-let spanEl = document.getElementById("updateContent");
-
-btnElement.onclick = function() {
-  let yourName = prompt("Please enter your name: ");
-  spanEl.textContent = yourName;
-}
--- practice-projects/random-number-generator/script.js
+++
@@ -1,5 +0,0 @@
-let n = document.getElementById("num");
-
-function generate_num(){
-  n.textContent = Math.floor(Math.random()*10000);
-}
