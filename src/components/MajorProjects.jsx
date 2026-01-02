e8ac329aea1f5ebb7969be833c9588737561dbec
jevita_pearl <jevitapearl@gmail.com>
2026-01-02 08:45:45 +0530

    Setup GitHub Actions for auto-deployment

--- src/components/MajorProjects.jsx
+++ src/components/MajorProjects.jsx
@@ -1,15 +1,18 @@
+import journalPic from "../assets/majorProjectImages/journal.png";
+import krytilPic from "../assets/majorProjectImages/krytil.png";
+
 function MajorProjects(){
   const majorprojects = [
     {
       id:"journal",
-      img: "src/assets/majorProjectImages/journal.png", 
+      img: journalPic, 
       title: "FrontEnd Template for a Journal App", 
       link: "https://jevitapearl.github.io/journal-app/index.html", 
       description: "This is my first Frontend project and was very helpful in getting my foundations on HTML and CSS very strong."
     },
     {
       id: "krytil",
-      img: "src/assets/majorProjectImages/krytil.png", 
+      img: krytilPic, 
       title: "Cover letter and ATS feature frontEnd", 
       link:"https://cv.krytil.com/", 
       description: "Built responsive, polished UI using modern frontend tools (React), designed intuitive form flows, and integrated with backend APIs to fetch/generate tailored cover-letter content."
